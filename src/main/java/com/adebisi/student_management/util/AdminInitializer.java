c0d2b81d54d65c2d9ed4da94a781d813b71c423f
Bisi_Cornelius <adebisiadeyemi@initsng.com>
2025-01-19 19:10:19 +0100

    first

--- src/main/java/com/adebisi/student_management/util/AdminInitializer.java
+++ src/main/java/com/adebisi/student_management/util/AdminInitializer.java
@@ -3,6 +3,7 @@ package com.adebisi.student_management.util;
 import com.adebisi.student_management.constant.Role;
 import com.adebisi.student_management.model.Users;
 import com.adebisi.student_management.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -14,6 +15,8 @@ import java.util.Optional;
 
 
 @Component
+
+@Slf4j
 public class AdminInitializer implements ApplicationRunner {
 
     private final UserRepository userRepository;
@@ -34,11 +37,14 @@ public class AdminInitializer implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 
         Optional<Users> user  = userRepository.findFirstByRole(Role.ADMIN);
+         log.info("user {}", user.isPresent());
+        log.info("email {}", adminEmail);
+        log.info("password {}", adminPassword);
         if(user.isEmpty()){
             Users admin = new Users();
             admin.setFirstName("Admin");
             admin.setLastName("Admin");
-            admin.setEmail(adminEmail);
+            admin.setEmail(adminEmail.trim());
             admin.setIsDisable(false);
             admin.setRole(Role.ADMIN);
             admin.setPassword(passwordEncoder.encode(adminPassword.trim()));
